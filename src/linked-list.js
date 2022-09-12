44aa68c32b2ec6b691b1eac2e0c6f1a94890a60d
Lippiece <lippiece@proton.me>
2022-09-12 22:53:37 +0200

    add ListNode constructor

--- src/linked-list.js
+++ src/linked-list.js
@@ -11,6 +11,18 @@ const LinkedList
         head,
         tail,
       } );
+/**
+ * It takes a data and returns an object
+ * with a next property set to undefined and a data property
+ * set to the data passed in
+ */
+const ListNode
+  = data =>
+    next =>
+      ( {
+        data,
+        next,
+      } );
 
     this.head = undefined;
     this.tail = undefined;
