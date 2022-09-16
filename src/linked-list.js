7b25e78f7cdc5a1f02857a598ea0fa6f1671c023
Lippiece <lippiece@proton.me>
2022-09-16 11:34:10 +0200

    expand the list functions
    add addAt(), slice(), toString(), find(),
    pop(), at(), shift()

--- src/linked-list.js
+++ src/linked-list.js
@@ -95,20 +95,158 @@ const append
       return reverse( prepended.head )( emptyList );
 
     };
-const print
+const toArray
     = currentNode =>
       arrayOfValues => {
 
         arrayOfValues.push( currentNode.data );
         if ( !currentNode.next ) { return arrayOfValues }
 
-        return print( currentNode.next )( arrayOfValues );
+        return toArray( currentNode.next )( arrayOfValues );
 
       };
+/**
+ * If the list has no head, return 0, otherwise, return
+ * the length of the array
+ *
+ * @returns The length of the list.
+ */
+const getSize
+  = list => {
+
+    if ( !list.head ) { return 0 }
+
+    return toArray( list.head )( [] ).length;
+
+  };
+/**
+ * If the list has a head, return a new list with the head's
+ * next node as the head and the tail as the tail.
+ *
+ * @returns A new list with the head removed.
+ */
+const shift
+  = list => {
+
+    if ( !list.head ) { return list }
+
+    return LinkedList( list.head.next )( list.tail );
+
+  };
+const pop
+  = list => {
+
+    if ( !list.head ) { return list }
+
+    const reversed = reverse( list.head )( emptyList );
+    const shifted  = shift( reversed );
+
+    return reverse( shifted.head )( emptyList );
+
+  };
+const at
+  = list =>
+    index => {
+
+      if ( !list.head ) { return "Empty" }
+
+      const arrayOfValues = toArray( list.head )( [] );
+
+      return arrayOfValues[ index ];
+
+    };
+const contains
+    = list =>
+      value => {
+
+        if ( !list.head ) { return false }
+
+        const arrayOfValues = toArray( list.head )( [] );
+
+        return arrayOfValues.includes( value );
+
+      };
+const find
+    = list =>
+      value => {
+
+        if ( !list.head ) { return "Empty" }
+
+        const arrayOfValues = toArray( list.head )( [] );
+
+        return arrayOfValues.indexOf( value );
+
+      };
+const toString
+    = list => {
+
+      if ( !list.head ) { return "Empty" }
+
+      const arrayOfValues = toArray( list.head )( [] );
+
+      return arrayOfValues.join( " -> " );
+
+    };
+const getToStart
+    = list =>
+      start => {
+
+        if ( start === 0 ) { return list }
+
+        return getToStart( shift( list ) )( start - 1 );
+
+      };
+const trimTheEnd
+    = list =>
+      end => {
+
+        if ( end === 0 ) { return list }
+
+        return trimTheEnd( pop( list ) )( end - 1 );
+
+      };
+const slice
+    = list =>
+      start =>
+        end => {
+
+          if ( start <= 0 ) {
+
+            return trimTheEnd( list )( getSize( list ) - end );
+
+          }
+
+          return trimTheEnd( getToStart( list )( start ) )( end - start );
+
+        };
+const restoreFrom
+    = list =>
+      values =>
+        currentIndex => {
+
+          if ( currentIndex < 0 ) { return list }
+          return restoreFrom( prepend( list )( values[ currentIndex ] ) )( values )( currentIndex - 1 );
+
+        };
+const addAt
+    = list =>
+      index =>
+        value => {
+
+          const sliced        = getToStart( list )( index );
+          const prepended     = prepend( sliced )( value );
+          const whatToRestore = toArray( list.head )( [] )
+            .slice( 0, index );
+
+          return restoreFrom( prepended )( whatToRestore )( whatToRestore.length - 1 );
+
+        };
 const list  = emptyList;
 const list2 = prepend( list )( 2 );
 const list3 = prepend( list2 )( 1 );
 const list4 = append( list3 )( 3 );
 const list5 = prepend( list4 )( -123 );
 console.log( list5 );
-console.log( print( list5.head )( [] ) );
+console.log( toString( list5 ) );
+const list6 = addAt( list5 )( 3 )( 999 );
+console.log( toString( list6 ) );
